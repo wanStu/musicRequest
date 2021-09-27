19f73ea034fe89870a9a3b58d1c643a03a1715a2
wcy <w_c_y_929@163.com>
2021-09-27 16:25:06 +0800

    数据库逆向迁移

---
+++ database/migrations/120210927162241_auth_group.php
@@ -0,0 +1,38 @@
+<?php
+
+use think\migration\Migrator;
+use think\migration\db\Column;
+use Phinx\Db\Adapter\MysqlAdapter;
+
+class AuthGroup extends Migrator
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $table = $this->table('auth_group', ['engine' => 'MyISAM', 'collation' => 'utf8_general_ci', 'comment' => '用户组表' ,'id' => 'id' ,'primary_key' => ['id']]);
+        $table->addColumn('title', 'char', ['limit' => 100,'null' => false,'default' => '','signed' => true,'comment' => '用户组中文名称',])
+			->addColumn('status', 'boolean', ['null' => false,'default' => 1,'signed' => true,'comment' => '状态：为1正常，为0禁用',])
+			->addColumn('rules', 'char', ['limit' => 80,'null' => false,'default' => '','signed' => true,'comment' => '用户组拥有的规则id， 多个规则","隔开，',])
+            ->create();
+    }
+}
---
+++ database/migrations/220210927162241_auth_group_access.php
@@ -0,0 +1,40 @@
+<?php
+
+use think\migration\Migrator;
+use think\migration\db\Column;
+use Phinx\Db\Adapter\MysqlAdapter;
+
+class AuthGroupAccess extends Migrator
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $table = $this->table('auth_group_access', ['engine' => 'MyISAM', 'collation' => 'utf8_general_ci', 'comment' => '用户组明细表' ,'id' => false ]);
+        $table->addColumn('uid', 'integer', ['limit' => MysqlAdapter::INT_MEDIUM,'null' => false,'default' => null,'signed' => false,'comment' => '用户id',])
+			->addColumn('group_id', 'integer', ['limit' => MysqlAdapter::INT_MEDIUM,'null' => false,'default' => null,'signed' => false,'comment' => '用户组id',])
+			->addIndex(['uid'], ['name' => 'uid'])
+			->addIndex(['uid','group_id'], ['unique' => true,'name' => 'uid_group_id'])
+			->addIndex(['group_id'], ['name' => 'group_id'])
+            ->create();
+    }
+}
